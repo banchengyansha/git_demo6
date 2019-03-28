8b82a4558eeba995d331fba53f6f3b10315e09f0
dengchangtao <deng.changtao@163.com>
2019-03-28 13:14:32 +0800

    first commit

---
+++ itcast_sms_service/src/main/java/cn/itcast/sms/Application.java
@@ -0,0 +1,16 @@
+package cn.itcast.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("----------------> app run1111 ....");
+		SpringApplication.run(Application.class, args);
+		System.out.println("-----------------> app end ....");
+		System.out.println("-----------------> app end444 ....");
+	}
+	
+}
